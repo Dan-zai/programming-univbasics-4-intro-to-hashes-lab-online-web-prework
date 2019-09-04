6a4a6c9fae8b289de9ae21038697f4d5761924b4
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-04 06:57:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,7 +28,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  key = "Steve"
+  key = "Tzvi"
+  read_from_hash = {:name = "Steve"}
 end
 
 def update_counting_hash(hash, key)
