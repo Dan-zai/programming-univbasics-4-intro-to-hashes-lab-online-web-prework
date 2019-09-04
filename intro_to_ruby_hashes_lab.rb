64cb7e95df3a1bcd110f18e298aa79dd87733426
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-04 07:20:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,7 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  monopoly = {
-    :name => {:name => "Steve", :names => "Tsvi"}
+  monopoly = 
 end
 
 def update_counting_hash(hash, key)
