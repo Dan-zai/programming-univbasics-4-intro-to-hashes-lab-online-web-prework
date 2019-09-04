3869e26236874a03be68a1673fca45518a3b9fd2
Daniel Cowpertwait <daniel.cowpertwait@gmail.com>
2019-09-04 07:04:38 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -43,6 +43,12 @@ describe "working with hashes" do
   end
 end
 
+describe "reading data from a hash" do
+  describe "read_from_hash" do
+    it "returns the value corresponding to the provided key" do
+      expect(read_from_hash({name: 'Steve'}, :name)).to eq('Steve')
+      expect(read_from_hash({'name' => 'Tzvi'}, 'name')).to eq('Tzvi')
+    end
 
     it "returns nil if the key is not found in the provided hash" do
       expect(read_from_hash({age: 31}, :name)).to eq(nil)
